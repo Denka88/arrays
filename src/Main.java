35ee7480f5b21076573c69b026252a1b3ba5502e
Denka88 <shadowfightdenka88@gmail.com>
2024-10-09 15:06:44 +0300

    Решена задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,55 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("Введите размер матрицы: ");
+        int n = scan.nextInt();
+
+
+        int[][] matrix = genMatrix(n);
+
+        for (int[] rows : matrix) {
+
+            for (int row : rows) {
+
+                System.out.print(row + " ");
+
+            }
+            System.out.println();
+
+        }
+    }
+
+    public static int[][] genMatrix(int n){
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 0; i < arr.length; i++){
+
+            for (int j = 0; j < arr[i].length; j++){
+
+                if (j == i){
+
+                    arr[i][j] = 2;
+
+                } else if (j < i) {
+
+                    arr[i][j] = 0;
+
+                }else {
+
+                    arr[i][j] = 1;
+
+                }
+
+            }
+
+        }
+
+        return arr;
 
     }
 }
